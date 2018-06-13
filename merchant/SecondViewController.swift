34218d4d46a2142629a3840aa92f3ffd25359258
Eugenio Mercado <cubymercado@me.com>
2018-06-13 17:00:38 +1200

    Initial Commit

---
+++ merchant/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  merchant
+//
+//  Created by Eugenio Mercado on 13/06/18.
+//  Copyright © 2018 Eugenio Mercado. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
